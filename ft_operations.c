94d8adee479b7ea68ba3f74d3407ad3cdd709a6e
Rigardt Engelbrecht <rengelbr@c3r8s1.wethinkcode.co.za>
2019-07-05 13:30:30 +0200

    k

--- ft_operations.c
+++ ft_operations.c
@@ -6,7 +6,7 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 13:15:15 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/07/05 13:26:41 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/07/05 13:30:22 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ int main(int ac, char *av[])
 	int 	i = 0;
 	int 	arg[ac - 1];
 
-	*arg = *av;
+	*arg = ()*av;
 	ft_striter(arg, ft_atoi());
 	stack = ft_stacknew(arg, ac);
 

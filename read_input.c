11a8a3c01256dd46320428f367f1a9097abc5954
Rigardt Engelbrecht <rengelbr@c3r5s2.wethinkcode.co.za>
2019-07-14 12:08:59 +0200

    changed params to accept pointer to pointer to head of list

--- read_input.c
+++ read_input.c
@@ -6,19 +6,20 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/12 10:35:11 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/07/12 10:57:12 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/07/14 11:49:30 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void    read_input(t_stack *a, t_stack *b, char **line)
+void    read_input(t_stack **a, t_stack **b, char **line)
 {
     while (get_next_line(0, line))
     {
         if (checkline(*line))
         {
-            do_op(*line, &a, &b);
+            ft_putendl("input accepted"); // remove later
+            do_op(*line, a, b); 
             free(*line);
         }
         else if (!(checkline(*line)))

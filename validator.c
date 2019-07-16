f0bd40f76697dae8bfa1b56a45ebb4f004f08d4d
Rigardt Engelbrecht <rengelbr@c3r5s2.wethinkcode.co.za>
2019-07-16 11:31:48 +0200

    string arg check added

--- validator.c
+++ validator.c
@@ -6,34 +6,38 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 12:27:56 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/07/15 16:18:33 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/07/16 11:13:21 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+// int     only_digits(char **str)
+// {
+
+// }
+
 int     input_valid(int ac, char **av)
 {
-    char    **args;
     int     i;
     int     j;
 
-    i = 0;
+    i = 1;
     j = 0;
     if (ac == 2)
     {
-        args = (char**)malloc(sizeof(char*) * (ft_wordcount(*av, ' ')));
-        args = ft_strsplit(*av, ' ');
-        while (ft_isdigit(args[i][j]))
+        while (av[i][j] != '\0')
         {
-            j = 0;
-            while (ft_isdigit(args[i][j]) && args[i][j] != '\0')
+            while (ft_isdigit(av[i][j]) || ft_iswhitespace(av[i][j]))
             {
-                j++;
-                if (!(*args))
+                while (ft_iswhitespace(av[i][j]))
+                    j++;
+                if (!(ft_isdigit(av[i][j])))
                     return (0);
+                if (av[i][j] == '\0')
+                    break ;
+                j++;
             }
-            i++;
         }
     }
     else if (ac > 2)

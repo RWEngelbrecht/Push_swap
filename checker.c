e0f59dccd6f9766cb825d737bae7662dbd43a7c3
Rigardt Engelbrecht <rengelbr@c3r5s2.wethinkcode.co.za>
2019-07-18 16:17:11 +0200

    changed for testing purposes, pushing for safety

--- checker.c
+++ checker.c
@@ -6,50 +6,12 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/08 11:46:43 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/07/16 12:38:34 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/07/18 12:18:51 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
-
-/* ************ Insertion sort experiment
-void	sortedInsert(t_stack **head, t_stack *new_head)
-{
-	t_stack *current;
-	if (*head == NULL || (*head)->value >= new_head->value)
-	{
-		new_head->next = *head;
-		*head = new_head;
-	}
-	else
-	{
-		// find the location before the point of insertion
-		current = *head;
-		while(current->next != NULL && current->next->value < new_head->value)
-		{
-			current = current->next; // point to next
-		}
-		new_head->next = current->next;
-		current->next = new_head;
-	}
-}
-
-void	insertionSort(t_stack **head)
-{
-	t_stack *sorted = NULL;
-	t_stack *current = *head;
-	t_stack *next;
-
-	while (current != NULL)
-	{
-		next = current->next;
-		sortedInsert(&sorted, current);
-		current = next;
-	}
-	*head = sorted;
-}
-*/
 
 int main(int argc, char *argv[])
 {
@@ -66,7 +28,7 @@ int main(int argc, char *argv[])
 		return (0);
 	}
 	/* Validate arguments */
-	if (input_valid(argc, argv))
+	if ((input_valid(argc, argv)))
 	{
 		stack_a = stackpopulate(argc, argv);
 		read_input(&stack_a, &stack_b, &line);
--- push_swap.c
+++ push_swap.c
@@ -6,18 +6,54 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 09:37:03 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/07/16 14:03:05 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/07/18 15:18:20 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+/*
+void	sortedInsert(t_stack **head, t_stack *new_head)
+{
+	t_stack *current;
+	if (*head == NULL || (*head)->value >= new_head->value)
+	{
+		new_head->next = *head;
+		*head = new_head;
+	}
+	else
+	{
+		// find the location before the point of insertion
+		current = *head;
+		while(current->next != NULL && current->next->value < new_head->value)
+		{
+			current = current->next; // point to next
+		}
+		new_head->next = current->next;
+		current->next = new_head;
+	}
+}
 
+void	insertionSort(t_stack **head)
+{
+	t_stack *sorted = NULL;
+	t_stack *current = *head;
+	t_stack *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		sortedInsert(&sorted, current);
+		current = next;
+	}
+	*head = sorted;
+}
+*/
 int main (int argc, char *argv[])
 {
     t_stack	*stack_a;
 	t_stack *stack_b;
 
-    char ops[4][4] = {"rra", "sa", "pb"};
+    char ops[4][4] = {"rrA", "sa", "pq"};
 
     stack_b = NULL;
 //    cmds = malloc(sizeof(t_ops) * 4);
@@ -30,7 +66,7 @@ int main (int argc, char *argv[])
     {
         stack_a = stackpopulate(argc, argv);
 		ft_putendl_fd(ops[1], 1);
-        ft_putendl_fd(ops[0], 1);
+		ft_putendl_fd(ops[2], 1);
     }
 //    ft_putendl_fd(ops[1], 1);
 //    ft_putendl_fd(ops[2], 1);

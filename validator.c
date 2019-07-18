0d060a926fb6c143ad70de09abe7bbb2003e755d
Rigardt Engelbrecht <rengelbr@c3r5s2.wethinkcode.co.za>
2019-07-18 16:20:31 +0200

    only_digit + duplicate checks added

--- validator.c
+++ validator.c
@@ -6,55 +6,84 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 12:27:56 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/07/16 15:40:11 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/07/18 16:15:57 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// int     only_digits(char **str)
-// {
+int     only_digits(char *str)
+{
+	while (*str)
+	{
+		if (!ft_isdigit(*str) && !ft_iswhitespace(*str))
+			return (0);
+		str++;
+	}
+	return (1);
+}
 
+int     dup_check(int ac, char **av)
+{
+    char **str;
+    int i;
+    int j;
+
+    i = 1;
+    str = NULL;
+    if (ac == 2)
+    {
+        str = ft_strsplit(av[1], ' ');
+        i = 0;
+    }
+    else
+        str = av;
+    while (str[i])
+    {
+        j = 1;
+        while (str[i + j] != '\0')
+        {
+            if (ft_strequ(str[i], str[i + j]))
+                return (0);
+            j++;
+        }
+        i++;
+    }
+    return (1);
+}
+
+// int     over_minmax(char **av)
+// {
+//     char *min;
+//     char *max;
+
+//     min = "-2147483649";
+//     max = "2147483648";
+//     while 
 // }
 
 int     input_valid(int ac, char **av)
 {
-    int     i;
-    int     j;
-
-    i = 1;
-    j = 0;
     if (ac == 2)
     {
-        while (av[i][j] != '\0')
-        {
-            while (ft_isdigit(av[i][j]) || ft_iswhitespace(av[i][j]))
-            {
-                while (ft_iswhitespace(av[i][j]) || av[i][j] == '-')
-                    j++;
-                if (!(ft_isdigit(av[i][j])))
-                    return (0);
-                if (av[i][j] == '\0')
-                    break ;
-                j++;
-            }
-        }
+        if (!only_digits(av[1]))
+			return (0);
+        if (!dup_check(ac, av))
+            return (0);
     }
     else if (ac > 2)
     {
-        while (av[i] != '\0')
+        int     i;
+
+        i = 1;
+        while (av[i])
         {
-            j = 0;
-            while (ft_isdigit(av[i][j]) && av[i][j] != '\0')
-            {
-                if (av[i][j] == '\0')
-                    break ;
-                if (!(ft_isdigit(av[i][j])))
-                    return (0);
-                j++;
-            }
+            if(!only_digits(av[i]))
+                return (0);
             i++;
         }
+        if (!dup_check(ac, av))
+            return (0);
     }
     return (1);
 }

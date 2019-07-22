9cb3d77e4195d96391ddc04b3ead26592b9c9e5e
Rigardt Engelbrecht <rengelbr@c3r5s2.wethinkcode.co.za>
2019-07-22 12:06:14 +0200

    changed return type to work better with error checking

--- read_input.c
+++ read_input.c
@@ -6,13 +6,13 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/12 10:35:11 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/07/16 14:29:19 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/07/22 09:32:12 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void    read_input(t_stack **a, t_stack **b, char **line)
+int    read_input(t_stack **a, t_stack **b, char **line)
 {
     while (get_next_line(0, line))
     {
@@ -20,12 +20,14 @@ void    read_input(t_stack **a, t_stack **b, char **line)
         {
             do_op(*line, a, b);
             free(*line);
+            return (1);
         }
         else if (!(checkline(*line)))
         {
             free(*line);
             //free stack
-            ft_putstr_fd("ERROR\n", 2);
+            ft_putstr_fd("Error\n", 2);
         }
     }
+    return (0);
 }

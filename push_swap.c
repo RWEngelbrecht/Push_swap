06d0fe57bb4bb508032013ce6cdc4c0cb83877bb
Rigardt Engelbrecht <rengelbr@c2r2s5.wethinkcode.co.za>
2019-08-08 14:41:01 +0200

    changed for testing purposes, pushing for safety

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 09:37:03 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/08/04 17:11:27 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/08/08 14:23:27 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,18 @@ int main (int argc, char *argv[])
 		if (is_sorted(stack_a = stackpopulate(argc, argv)))
 			return (0);
 		stacklen = stack_len(&stack_a);
+//printf("\n STACK A:\n");	//REMOVE
+//print_stack(&stack_a);		//REMOVE
+
 		if (stacklen == 3)
 			do_sort_three(&stack_a, &stack_b);
-		 else if (stacklen == 5)
+		 else if (stacklen >= 5)
 		 	do_sort_five(&stack_a, &stack_b);
-		
 	//	quicksort(stacklen, &stack_a, &stack_b);
 	}
-	// printf("\n STACK A:\n");	//REMOVE
-	// print_stack(&stack_a);		//REMOVE
-	// printf("\n STACK B:\n");	//REMOVE
-	// print_stack(&stack_b);		//REMOVE
+//	 printf("\n STACK A:\n");	//REMOVE
+//	 print_stack(&stack_a);		//REMOVE
+//	 printf("\n STACK B:\n");	//REMOVE
+//	 print_stack(&stack_b);		//REMOVE
 	return (0);
 }

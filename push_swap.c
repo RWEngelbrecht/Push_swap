5170539466ff17a249d55a094f50a8704fabd092
Rigardt Engelbrecht <rengelbr@c3r5s5.wethinkcode.co.za>
2019-08-09 16:36:14 +0200

    now calls do_sort_5 for stacks > 3

--- push_swap.c
+++ push_swap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rengelbr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 09:37:03 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/08/08 14:23:27 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/08/09 16:26:36 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,15 +30,19 @@ int main (int argc, char *argv[])
 		if (is_sorted(stack_a = stackpopulate(argc, argv)))
 			return (0);
 		stacklen = stack_len(&stack_a);
+// printf("\n STACK A:\n");	//REMOVE
+// print_stack(&stack_a);		//REMOVE
+// printf("\n STACK B:\n");	//REMOVE
+// print_stack(&stack_b);		//REMOVE
 		if (stacklen == 3)
 			do_sort_three(&stack_a, &stack_b);
-		 else if (stacklen >= 5)
+		 else if (stacklen > 3)
 		 	do_sort_five(&stack_a, &stack_b);
 	//	quicksort(stacklen, &stack_a, &stack_b);
 	}
-//	 printf("\n STACK A:\n");	//REMOVE
-//	 print_stack(&stack_a);		//REMOVE
-//	 printf("\n STACK B:\n");	//REMOVE
-//	 print_stack(&stack_b);		//REMOVE
+// printf("\n STACK A:\n");	//REMOVE
+// print_stack(&stack_a);		//REMOVE
+// printf("\n STACK B:\n");	//REMOVE
+// print_stack(&stack_b);		//REMOVE
 	return (0);
 }

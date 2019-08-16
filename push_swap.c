92ba9fe8691f1c13c158d72c78914e6b1cf8e8f1
Rigardt Engelbrecht <rengelbr@c3r5s2.wethinkcode.co.za>
2019-08-16 13:05:35 +0200

    else if statements to handle specific ranges added. move to seperate funct

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 09:37:03 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/08/16 08:29:50 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/08/16 12:14:12 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,16 @@ int			main(int argc, char *argv[])
 			do_sort_three(&stack_a, &stack_b);
 		else if (stacklen > 3 && stacklen <= 5)
 			do_sort_five(&stack_a, &stack_b);
-		else if (stacklen > 5)
+		else if (stacklen > 5 && stacklen <= 20)
 			do_sort_twenty(&stack_a, &stack_b);
+		else if (stacklen > 20 && stacklen <= 50)
+			do_sort_fifty(&stack_a, &stack_b);
+		else if (stacklen > 50 && stacklen <= 100)
+			do_sort_hundred(&stack_a, &stack_b);
+		else if (stacklen > 100 && stacklen <= 250)
+			do_sort_250(&stack_a, &stack_b);
+		else if (stacklen > 250 && stacklen <= 500)
+			do_sort_500(&stack_a, &stack_b);
 	}
 // printf("\n STACK A:\n");	//REMOVE
 // print_stack(&stack_a);		//REMOVE

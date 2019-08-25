f4e729dc5bd0b23d72ebcf8f54973915e5327810
RWEngelbrecht <rigardt@brechtburg.co.za>
2019-08-25 09:23:55 +0200

    no longer work directly with argv, or leaks. made choose_sort params double ptr to stacks.

--- push_swap.c
+++ push_swap.c
@@ -3,51 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rengelbr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 09:37:03 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/08/23 14:26:37 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/08/25 09:10:26 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	choose_sort(t_stack *a, t_stack *b, int stacklen)
+static void	choose_sort(t_stack **a, t_stack **b, int stacklen)	///made 2x ptr
 {
 	if (stacklen == 2)
-		do_sort_two(&a, &b);
+		do_sort_two(a, b);
 	else if (stacklen == 3)
-		do_sort_three(&a, &b);
+		do_sort_three(a, b);
 	else if (stacklen > 3 && stacklen <= 5)
-		do_sort_five(&a, &b);
+		do_sort_five(a, b);
 	else if (stacklen > 5 && stacklen <= 20)
-		do_sort_twenty(&a, &b);
+		do_sort_twenty(a, b);
 	else if (stacklen > 20 && stacklen <= 50)
-		do_sort_fifty(&a, &b);
+		do_sort_fifty(a, b);
 	else if (stacklen > 50 && stacklen <= 105)
-		do_sort_hundred(&a, &b);
+		do_sort_hundred(a, b);
 	else if (stacklen > 100 && stacklen <= 250)
-		do_sort_250(&a, &b);
+		do_sort_250(a, b);
 	else if (stacklen > 250 && stacklen <= 505)
-		do_sort_500(&a, &b);
+		do_sort_500(a, b);
 	else if (stacklen > 505)
-		do_sort_large(&a, &b);
+		do_sort_large(a, b);
 }
 
 int			main(int argc, char *argv[])
 {
 	t_stack	*stack_a;
 	t_stack	*stack_b;
+	char **args;
 
 	stack_a = NULL;
 	stack_b = NULL;
+	args = argv;
 	if (argc < 2)
 		ERROR;
-	if (input_valid(argc, argv))
+	if (input_valid(argc, args))
 	{
-		if (is_sorted(stack_a = stackpopulate(argc, argv)))
+		stack_a = stackpopulate(argc, args);
+		if (is_sorted(stack_a))
 			return (0);
-		choose_sort(stack_a, stack_b, stack_len(&stack_a));
+		choose_sort(&stack_a, &stack_b, stack_len(&stack_a));
 	}
 	return (0);
 }

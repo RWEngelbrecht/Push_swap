0501c2f942742a499d4916f27b147cb9757b7d02
RWEngelbrecht <rigardt@brechtburg.co.za>
2019-08-25 09:15:27 +0200

    found last leak in normalize. was allocating same node twice

--- checker.c
+++ checker.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   checker.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rengelbr <rengelbr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rengelbr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/08 11:46:43 by rengelbr          #+#    #+#             */
-/*   Updated: 2019/08/23 14:26:32 by rengelbr         ###   ########.fr       */
+/*   Updated: 2019/08/25 09:08:34 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,6 @@ int	main(int argc, char *argv[])
 	char **args;
 
 	stack_a = NULL;
-	//stack_a = stack_new(0);
-	//line = NULL;
-	//stack_b = stack_new(0);
 	args = argv;
 	stack_b = NULL;
 	if (argc < 2)
@@ -30,7 +27,6 @@ int	main(int argc, char *argv[])
 	if ((input_valid(argc, args)))
 	{
 		stack_a = stackpopulate(argc, args);
-//		while(1);							////////////////////WHILE HERE
 		if (!read_input(&stack_a, &stack_b, &line))
 		{
 			if (stack_a == malloc(sizeof(t_stack)))
@@ -42,10 +38,6 @@ int	main(int argc, char *argv[])
 		ERROR;
 	if (is_sorted(stack_a) && !stack_b)
 	{
-ft_putstr("\nstack_a:\n");
-print_stack(&stack_a);
-ft_putstr("\nstack_b:\n");
-print_stack(&stack_b);
 		free(stack_a);
 		free(stack_b);
 		ft_putstr_fd("OK\n", 1);
@@ -53,8 +45,6 @@ print_stack(&stack_b);
 	}
 	else
 	{
-ft_putstr("stack_a:\n");
-print_stack(&stack_a);
 		free(stack_a);    ////
 		free(stack_b);    ////
 		ft_putstr_fd("KO\n", 1);
